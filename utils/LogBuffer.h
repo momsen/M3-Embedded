c18a7fe2b35ce32b707f98e1a5679826de3bf9df
Maurice-Marie Stromer <mms@ppi.de>
2019-10-22 16:06:54 +0200

    WIP logbuffer

--- utils/LogBuffer.h
+++ utils/LogBuffer.h
@@ -3,82 +3,68 @@
 
 #include <Arduino.h>
 
-#define BYTES_PER_ROW (withHexBuffer ? (2 + charsPerRow * 6) : (charsPerRow + 1))
-
 namespace utils
 {
 
-template <uint8_t charsPerRow, uint8_t maxRows, bool withHexBuffer>
+template <uint32_t maxSize>
 class LogBuffer
 {
 public:
-  LogBuffer(const char* headline)
-    : rows(0),
-      headlineLength(strlen(headline)),
-      content(String(headline)) {
+  LogBuffer(const char* headline) {
+    memset(content, 0, maxSize);
+    headlineLength = strlen(headline);
+    strcpy(headline, content);
+    content[headlineLength++] = '\n';
+    contentPtr = headlineLength;
   }
 
   void appendMessage(String message) {
-    appendMessage((uint8_t*)message.c_str(), message.length());
+    appendMessage(message.c_str());
   }
 
   void appendMessage(const char* string) {
-    appendMessage((uint8_t*)string, strlen(string));
-  }
-
-  void appendMessage(uint8_t* pkg, uint32_t len) {
-    int ptr = 0;
-    while(ptr < len) {
-        appendRow(pkg + ptr, min(charsPerRow, (uint8_t)(len - ptr)));
-        ptr += charsPerRow;
+    char* input = string;
+    while(*input != '\0') {
+      appendChar(*input);
     }
   }
 
-  void appendRow(uint8_t* chars, uint8_t len) {
-    if(rows >= maxRows) {
-        content.remove(headlineLength, BYTES_PER_ROW);
-        rows--;
+  void appendChar(const char c) {
+    if(contentPtr >= maxSize) {
+      wrap();
     }
 
-    for(int i = 0; i < len; i++) {
-        content += isChar(chars[i]) ? (char)chars[i] : '.';
-    }
-    
-    for(int i = 0; i < charsPerRow - len; i++) {
-        content += " ";
-    }
-    
-    if(withHexBuffer) {
-      content += "\t";
-
-      for(int i = 0; i < len; i++) {
-          content += chars[i] < 0x10 ? " 0x0" : " 0x";
-          content += String(chars[i], HEX);
-      }
-      
-      for(int i = 0; i < charsPerRow - len; i++) {
-          content += "     ";
-      }
-    }
-
-    content += "\n";
-    rows++;
+    content[contentPtr++] = c;
   }
 
-  bool isChar(uint8_t c) {
-    return c == ' ' || c == ':' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+  void newline() {
+    appendChar('\n');
   }
 
-  String get_content() {
+  String getContent() {
       return content;
   }
 
 private:
-    int headlineLength;
-    String content;
-    String hexContent;
-    String binaryContent;
-    int rows;
+  void wrap() {
+    uint32_t firstNewLine = headlineLength;
+    while(content[firstNewLine++] != '\n' && firstNewLine < maxSize) {
+    }
+
+    if(firstNewLine >= maxSize) {
+      // no \n found, using removing first 100 bytes
+      firstNewLine = headlineLength + 100;
+    }
+
+    memcpy(content + headlineLength, content + firstNewLine, firstNewLine * sizeof(char));
+    memset(content + maxSize - firstNewLine, 0, firstNewLine * sizeof(char));
+    contentPtr -= firstNewLine;
+  }
+
+  uint8_t headlineLength;
+  char content[charsPerRow * maxRows];
+  uint8_t x;
+  uint8_t y;
 };
 
 } // namespace utils
